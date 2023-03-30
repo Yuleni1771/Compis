e3dc35ed33e79c22daaa5153448013564b95234a
Yuleni1771 <73555981+Yuleni1771@users.noreply.github.com>
2023-03-30 11:49:07 -0600

    Add files via upload

--- Auto.java
+++ Auto.java
@@ -17,7 +17,7 @@ public class Auto {
    public static void main(String[] args) {
       Auto c1 = new Auto("Honda", 2008, "rojo");
       Auto c2 = c1;
-      Auto c3 = new Auto("Honda", 2008, "amatillo");
+      Auto c3 = new Auto("Honda", 2008, "rojo");
       
       System.out.println(c2 == c1);
       System.out.println(c2.equals(c3));
